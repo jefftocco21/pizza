394f3f50fdaf9fe7941886c19854aafd363128be
jnt369@gmail.com <jeff.tocco@lyonsexpress.com>
2022-05-10 09:53:30 -0400

    API test by passing in multi-dimensional PHP array parsed as JSON

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/posts/{id}', function($id) {
+    return response('Post ' . $id);
+})->where('id', '[0-9]+');
+
+Route::get('/search', function(Request $request){
+    return($request->name . ' ' . $request->city);
 });
