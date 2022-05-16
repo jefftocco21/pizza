8fa1b97d554ea95c915fe68bf6fa403403ff265a
jnt369@gmail.com <jeff.tocco@lyonsexpress.com>
2022-05-16 12:23:08 -0400

    Changing logo color

--- resources/views/partials/_hero.blade.php
+++ resources/views/partials/_hero.blade.php
@@ -7,8 +7,8 @@ class="relative h-72 bg-orange-100 flex flex-col justify-center align-center tex
 ></div>
 
 <div class="z-10">
-    <h1 class="text-6xl font-bold uppercase text-orange-400">
-        PHP<span class="text-black">izza</span>
+    <h1 class="text-6xl font-bold uppercase text-black">
+        PHP<span class="text-orange-400">izza</span>
     </h1>
     <p class="text-2xl text-gray-200 font-bold my-4">
     </p>
