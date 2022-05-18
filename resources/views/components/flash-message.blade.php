100eda9a68211cc4177e6efc7c67f7fdeceb2a68
jnt369@gmail.com <jeff.tocco@lyonsexpress.com>
2022-05-18 09:16:16 -0400

    Update flash message to softer color, add index anchor to layout nav

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,6 +1,6 @@
 @if(session()->has('message'))
     <div x-data="{show: true}" x-init="setTimeout(() => show = false, 3000)" x-show="show"
-     class="fixed top-0 left-1/2 transform -translate-x-1/2 bg-orange-500 text-white px-48 py-3">
+     class="fixed top-0 left-1/2 transform -translate-x-1/2 bg-orange-300 text-white px-48 py-3">
         <p>
             {{session('message')}}
         </p>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -26,8 +26,8 @@
         </head>
         <body class="mb-48">
             <nav class="flex justify-between items-center mb-4">
-                <a href="index.html"
-                    ><img class="w-24" src="images/logo.png" alt="" class="logo"
+                <a href="/"
+                    ><img class="w-24 ml-4" src="images/logo.png" alt="PHPizza" class="logo"
                 /></a>
                 <ul class="flex space-x-6 mr-6 text-lg">
                     <li>
